2db77db906707cf32e85f60496249928a2256ddf
Bigboss6797976 <@example.com>
2026-06-05 23:55:03 +0800

    fix: use requests+flask only, remove pydantic

--- games/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dice import DiceGame
-from .slots import SlotsGame
-from .coin import CoinGame
-from .roulette import RouletteGame
-from .crash import CrashGame
-
-__all__ = ["DiceGame", "SlotsGame", "CoinGame", "RouletteGame", "CrashGame"]
